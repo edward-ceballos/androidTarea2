c80a59637aa62321c1f9f8f41eed2964896552cb
Edward Ceballos <edwardc08@hotmail.com>
2018-11-22 13:39:53 -0400

    Relative Layout

--- app/src/main/java/com/edwardceballos/androidtarea2/LinearLayoutHorizontal.java
+++ app/src/main/java/com/edwardceballos/androidtarea2/LinearLayoutHorizontal.java
@@ -1,11 +1,14 @@
 package com.edwardceballos.androidtarea2;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class LinearLayoutHorizontal extends AppCompatActivity {
 
@@ -16,6 +19,31 @@ public class LinearLayoutHorizontal extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button button7 = (Button) findViewById(R.id.button7);
+        Button button8 = (Button) findViewById(R.id.button8);
+        Button button9 = (Button) findViewById(R.id.button9);
+        Button button10 = (Button) findViewById(R.id.button10);
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+            }
+        });
+        button9.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getWindow().getDecorView().setBackgroundColor(Color.RED);
+            }
+        });
+        button10.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/edwardceballos/androidtarea2/RelativeLayout.java
+++ app/src/main/java/com/edwardceballos/androidtarea2/RelativeLayout.java
@@ -1,28 +1,29 @@
 package com.edwardceballos.androidtarea2;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class linear_layout_vertical extends AppCompatActivity {
+public class RelativeLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_linear_layout_vertical);
+        setContentView(R.layout.activity_relative_layout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
-    public void viewNotification(View view){
-        Toast.makeText(getApplicationContext(), "Notificación tarea 2" , Toast.LENGTH_SHORT ).show();
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
 }
